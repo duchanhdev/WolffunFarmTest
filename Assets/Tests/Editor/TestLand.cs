f0715fae22094cb3fb3930a91215fe6b4c1c7e32
Hanh.Nguyen <duchanhdev@gmail.com>
2025-04-28 10:49:37 +0700

    Add Unit Test

---
+++ Assets/Tests/Editor/TestLand.cs
@@ -0,0 +1,26 @@
+﻿using Core.Manager;
+using NUnit.Framework;
+
+namespace Tests.Editor
+{
+    public class TestLand
+    {
+        [Test]
+        public void TestNumLandFirst()
+        {
+            Assert.AreEqual(GameManager.Instance.LandManager.TotalLand,
+                GameManager.Instance.Configs.GlobalConfig.GetInt("Land_First"));
+        }
+        
+        [Test]
+        public void TestAddLand()
+        {
+            var oldNum = GameManager.Instance.LandManager.TotalLand;
+            var price = GameManager.Instance.Configs.GlobalConfig.GetInt("Land_ExpansionPrice");
+            GameManager.Instance.PlayerResources.AddGold(price);
+            GameManager.Instance.ExpandLand();
+            var newNum = GameManager.Instance.LandManager.TotalLand;
+            Assert.AreEqual(oldNum + 1, newNum);
+        }
+    }
+}
